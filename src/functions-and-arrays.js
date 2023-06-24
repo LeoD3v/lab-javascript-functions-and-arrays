127c0e64607dcf1e261571e81a7b7460600f6ed8
Leandro Rodriguez <leoargusa1991@gmail.com>
2023-06-24 16:23:04 +0200

    find the maximum

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,6 +1,10 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {
-  sdf;
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
 }
 
 // Iteration #2: Find longest word
